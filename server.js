fa08228aa1760c6c68937d97df755a2237535f88
Nazia R <nazia.huma@gmail.com>
2021-07-28 01:24:17 +0400

    refactor code in server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const DB_NAME = process.env.DB_NAME || "budget";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -19,10 +20,11 @@ const MONGODB_URI = process.env.MONGOATLAS_URI || "mongodb://localhost/budget";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
